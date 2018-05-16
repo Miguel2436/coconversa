648ef98ea8e7ea87dc99497d5bdbf6a3565d08a8
Alansito <33964995+Alansito@users.noreply.github.com>
2018-05-16 09:07:12 -0500

    JavaDoc completo.

--- Coconversa/src/coconversa/FormUsuarioEncontrado.java
+++ Coconversa/src/coconversa/FormUsuarioEncontrado.java
@@ -17,7 +17,18 @@ public class FormUsuarioEncontrado extends JFrame implements ActionListener
     private JButton btnSiAgregarUsuario, btnNoAgregarUsuario;
     private String User,Amigo;
     private ObjectOutputStream OOS;
-    public FormUsuarioEncontrado(ObjectOutputStream OOS,String Usuario,String amigo)
+    
+    /**
+     * Inicializa el Form mandando a llamar la función "configurar" y "componentes".
+     * Este Form se manda a llamar cuando un usuario ha sido encontrado y se le quiere mandar solicitud de amistad.
+     * @param OOS
+     * Es el objeto por el cual se escribe al socket.
+     * @param Usuario
+     * Es un String con el nombre del usuario que va a mandar la solicitud.
+     * @param amigo 
+     * Es un String con el nombre del usuario al que le llegará la solicitud de amistad.
+     */
+    public FormUsuarioEncontrado(ObjectOutputStream OOS, String Usuario, String amigo)
     {
         this.OOS = OOS;
         this.User = Usuario;
@@ -26,6 +37,9 @@ public class FormUsuarioEncontrado extends JFrame implements ActionListener
         componentes(Usuario);
     }
     
+    /**
+     * En esta función se configura el nombre y tamaño de la ventana, se centra la venatana en la pantalla y se inhabilita la opción de mover manualmente su tamaño.
+     */
     public void configurar()
     {
         this.setTitle("Buscar Amigo");
@@ -34,6 +48,11 @@ public class FormUsuarioEncontrado extends JFrame implements ActionListener
         this.setResizable(false);   
     }
     
+    /**
+     * En esta función se inicializan todos los componentes y se acomodan en sus respectivos grupos para generar el diseño de la ventana.
+     * @param Usuario
+     * Es un String con el nombre del usuario encontrado para mostrarse en un JLabel.
+     */
     public void componentes(String Usuario)
     {
         lblUsuarioEncontrado = new JLabel("¡El usuario " + User + " ha sido encontrado!");
@@ -80,7 +99,12 @@ public class FormUsuarioEncontrado extends JFrame implements ActionListener
         this.pack();
         
     }
-
+    
+    /**
+     * Esta función es mandada a llamar cuando se presiona un botón y dependiendo de cual haya sido presionado se ejecutará una parte de código u otra.
+     * @param ae 
+     * Es el "identificador" del botón que fue presionado. Con "ae" se evalúa cual botón mandó a llamar la función para ejecutar cierto código.
+     */
     @Override
     public void actionPerformed(ActionEvent ae) 
     {
@@ -89,12 +113,10 @@ public class FormUsuarioEncontrado extends JFrame implements ActionListener
           ClienteHiloEscritura findFriend = new ClienteHiloEscritura(OOS);
           findFriend.agregarAmigo(User,Amigo);
           this.setVisible(false);
-          
         }
         if (ae.getSource()==btnNoAgregarUsuario)
         {
             this.setVisible(false);
         }
     }
-    
 }
