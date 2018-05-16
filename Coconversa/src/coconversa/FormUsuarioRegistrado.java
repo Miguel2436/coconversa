1a0395ae7ce6a740208c2b355b8008b0a3cab919
Alansito <33964995+Alansito@users.noreply.github.com>
2018-05-16 09:08:35 -0500

    JavaDoc completo.

--- Coconversa/src/coconversa/FormUsuarioRegistrado.java
+++ Coconversa/src/coconversa/FormUsuarioRegistrado.java
@@ -17,6 +17,12 @@ public class FormUsuarioRegistrado extends JFrame implements ActionListener
     private JButton btnIniciarSesion;
     private ObjectOutputStream OOS;
     
+    /**
+     * Inicializa el Form mandando a llamar la función "configurar" y "componentes".
+     * Este Form se manda a llamar si el usuario se ha regitrado correctamente en el sistema en el Form de SignUp.
+     * @param OOS
+     * Es el objeto por el cual se escribe al socket.
+     */
     public FormUsuarioRegistrado(ObjectOutputStream OOS)
     {
         this.OOS = OOS;
@@ -24,6 +30,9 @@ public class FormUsuarioRegistrado extends JFrame implements ActionListener
         componentes();
     }
     
+    /**
+     * En esta función se configura el nombre y tamaño de la ventana, se centra la venatana en la pantalla y se inhabilita la opción de mover manualmente su tamaño.
+     */
     public void configurar()
     {
         this.setTitle("SignUp");
@@ -32,6 +41,9 @@ public class FormUsuarioRegistrado extends JFrame implements ActionListener
         this.setResizable(false); 
     }
     
+    /**
+     * En esta función se inicializan todos los componentes y se acomodan en sus respectivos grupos para generar el diseño de la ventana.
+     */
     public void componentes()
     {
         lblUsuarioRegistrado = new JLabel("Usuario registrado de manera");
@@ -64,6 +76,11 @@ public class FormUsuarioRegistrado extends JFrame implements ActionListener
         this.pack();
     }
 
+    /**
+     * Esta función es mandada a llamar cuando se presiona un botón y dependiendo de cual haya sido presionado se ejecutará una parte de código u otra.
+     * @param ae
+     * Es el "identificador" del botón que fue presionado. Con "ae" se evalúa cual botón mandó a llamar la función para ejecutar cierto código.
+     */
     @Override
     public void actionPerformed(ActionEvent ae) 
     {
